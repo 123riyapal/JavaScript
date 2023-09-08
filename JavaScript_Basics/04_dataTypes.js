2e8747161159cb36e474e7fe86439ebf5856a136
Riya Pal <95326631+123riyapal@users.noreply.github.com>
2023-09-08 17:23:45 +0000

    datatypes in JS

---
+++ JavaScript_Basics/04_dataTypes.js
@@ -0,0 +1,22 @@
+// primitve data types
+
+// Number ,null , string ,symbol, boolean , bigint ,undefined
+
+// Refernce or Non - primitive  data types
+
+// Array
+// Object
+// Function
+
+let arr=[1,2,3];
+let obj={
+    "Riya": 75,
+    "Shagun":70,
+    "Khushi":65
+}
+function myFunction(val){
+return val;
+}
+console.log(arr);
+console.log(obj)
+console.log(myFunction(3));
