72dace81d338bee7521b8967214f8906dc3e2379
Riya Pal <95326631+123riyapal@users.noreply.github.com>
2023-09-11 06:55:01 +0000

    date in JS

--- JavaScript_Basics/07_dats.js
+++ JavaScript_Basics/07_dats.js
@@ -1,3 +1,9 @@
 const myDate=new Date();
-console.log(myDate.toJSON())
-console.log(typeof myDate);
+// console.log(myDate.toJSON())
+// console.log(typeof myDate);
+
+console.log(myDate.getFullYear());
+console.log(myDate.toLocaleString('default', {
+    weekday:"long",
+    month:"numeric",
+}));
