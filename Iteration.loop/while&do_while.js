c0aa42b7772bd9ef86479ea54ecf8a3bfd062fad
Riya Pal <95326631+123riyapal@users.noreply.github.com>
2023-09-18 05:54:15 +0000

    while/dowhile loop

---
+++ Iteration.loop/while&do_while.js
@@ -0,0 +1,11 @@
+const coding=["js","cpp","c","perl","swift","rubby"];
+let i=0;
+while(i<coding.length){
+    console.log(coding[i]);
+    i++;
+}
+let j=0;
+do{
+    console.log(j);
+    j++
+}while(j<10);
